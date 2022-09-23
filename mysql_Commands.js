1a68b2f1dc84ba9ad3593f44cfba5a3c52400e5c
Merijn <99054535@mydavinci.nl>
2022-09-23 14:13:52 +0200

    currently figuring out how to add new table's in the database making good progress

--- mysql_Commands.js
+++ mysql_Commands.js
@@ -9,12 +9,23 @@ var allOrders = "";
 database.query("SELECT * FROM orders", function (err, result, fields) {
     if (err) throw err;
     console.log(result);
+    // console.log(fields);
     allOrders = result;
     console.log(allOrders + " A vibe check");
 
 });
+
+var customerName = "Rober Genneweg";
+
+database.query("INSERT INTO orders (customer_name, customer_id, order_price, order_gram) VALUES ('" + customerName + "', '" + customerID + "', '" + orderPrice + "', '" + orderGram + "')", function (err, result) {
+    if (err) throw err;
+    console.log(result);
+    // allOrders = result;
+    // console.log(allOrders + " A vibe check");
+
+});
 // console.log(TestingName);
-console.log(allOrders + " A vibe check");
+console.log(allOrders + " A vibe check part 2");
 
 // con.query("SELECT * FROM orders", function (err, result, fields) {
 //     if (err) throw err;
