f320f28b95d7ebe673e2dfd04278bddb0de554c8
Merijn <99054535@mydavinci.nl>
2022-10-10 11:03:26 +0200

    added Select2 and changed the php files to work more likely

--- commands_db.php
+++ commands_db.php
@@ -1,4 +1,13 @@
 <?php
-require("connectionDb.php");
+require "connectionDb.php";
 
 console.log("it works");
+
+function getAllOrders(){
+    $conn = dbConnect();
+    $stmt = $conn->prepare("SELECT * FROM orders");
+    $stmt->execute();
+
+    $result = $stmt->setFetchMode(PDO::FETCH_ASSOC);
+    return $stmt->fetchAll();
+}
--- connectionDb.php
+++ connectionDb.php
@@ -1,14 +1,16 @@
 <?php
-$servername = "localhost";
-$username = "root";
-$password = "mysql";
-$dbname = "bonzai_boom_reis";
+function dbConnect(){
+  $servername = "localhost";
+  $username = "root";
+  $password = "mysql";
+  $dbname = "bonzai_boom_reis";
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
-  } catch(PDOException $e) {
-    echo "Connection failed: " . $e->getMessage();
-  }
+  try {
+      $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+      // set the PDO error mode to exception
+      $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+      echo "Connected successfully";
+    } catch(PDOException $e) {
+      echo "Connection failed: " . $e->getMessage();
+    }
+}
